4002da2670fd81dcec89c31d26ab8a4bd9be0e7f
Artem Shakhbazian <shakhbazianartem@gmail.com>
2022-06-26 16:33:36 +0300

    TASK-4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-import prompt
 
 from brain_games.cli import welcome_user
 
-def welcome ():
-    print ("Welcome to the Brain Games!")
+
+def welcome():
+    print("Welcome to the Brain Games!")
 
 
-
-def main ():
+def main():
     welcome()
     welcome_user()
+
 
 if __name__ == "__main__":
     main()
