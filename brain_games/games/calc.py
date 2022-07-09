9b0334a33c434e91aa5e82321d418736de65f8a2
Artem Shakhbazian <shakhbazianartem@gmail.com>
2022-07-09 15:52:43 +0300

    Refactoring

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,7 +5,7 @@ import operator
 ops = {'+': operator.add,
        '-': operator.sub,
        '*': operator.mul,
-       '/': operator.truediv}
+       '//': operator.floordiv}
 
 
 TASK = 'What is the result of the expression?'
