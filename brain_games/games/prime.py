90dbe650437f9a47b140cc997272438f0317ba2e
Artem Shakhbazian <shakhbazianartem@gmail.com>
2022-07-09 16:25:36 +0300

    Refactoring

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,9 +6,12 @@ TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def game_script():
     value = (random.randint(1, 100))
+    k = 0
     for i in range(2, (value // 2 + 1)):
         if (value % i == 0):
-            answer = 'no'
-        else:
-            answer = 'yes'
+            k += 1
+    if (k <= 0):
+        answer = 'yes'
+    else:
+        answer = 'no'
     return answer, value
